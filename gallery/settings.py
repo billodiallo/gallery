60c2fd726e093f5765c720fc00bccf999c7759ad
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-05-25 17:50:45 +0300

    Building path inside the project

--- gallery/settings.py
+++ gallery/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+######BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
