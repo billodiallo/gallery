48c46a970d1407d0b6047a4ce35e5bb3717e2bfd
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-05-25 17:34:05 +0300

    update fashion function

--- photo/views.py
+++ photo/views.py
@@ -7,3 +7,29 @@ def fashion(request):
     images = Image.objects.all()
     return render(request, 'fashion.html',{'images':images})
 
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        search_photos = Image.search_by_categories(search_term) 
+        message = f"{search_term}"
+        return render(request, 'all-photo/search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        message = "You haven't searched for any term "
+        return render(request, 'all-photo/search.html',{"message":message})
+
+def image(request,image_id):
+         try:
+             image = Image.objects.get(id = image_id)
+
+         except DoesNotExist:
+             raise Http404() 
+         return render(request,"all-photo/image.html",{"images":image})
+
+def filter_by_location(request,location_id):
+        """
+        Function that filters images by location
+        """
+        images = Image.filter_by_location(id=location_id )
+        return render (request, 'location.html', {"images":images})
+   
